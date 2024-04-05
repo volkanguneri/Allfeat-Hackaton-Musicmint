a44750befb348b326a411287cb34c879d9c185db
adrien-vieilleribiere <adrien.v@gmail.com>
2024-04-05 10:17:51 +0200

    Add front - wagmi (#10)
    * frontend
    * wagmi viem for addSuperAdmin
    ---------
    Co-authored-by: root <root@DESKTOP-IA0TRAI>

--- frontend/app/admin/page.js
+++ frontend/app/admin/page.js
@@ -14,26 +14,109 @@ import { parseAbiItem } from "viem";
 import { usePublicClient } from "wagmi";
 import { hardhat } from "viem/chains";
 
+// Contract's information
+import { AdminsABI, contractAddressAdmins } from "@/constants/index";
+
 const admin = () => {
+  // Admin Information
+
+  const [superAdmin, setSuperAdmin] = useState("");
+
+  // Wagmi function / client creation for event listenning
+  const client = usePublicClient();
+
+  // Event information
+  const [grantedEvents, setGrantedEvents] = useState([]);
+
+  // Event handling function
+  const getGrantedEvents = async () => {
+    try {
+      // get.Logs from viem
+      const logs = await client.getLogs({
+        address: contractAddressAdmins,
+        event: parseAbiItem(
+          "event Granted (address from, address to, Role role,address contractt);"
+        ),
+        fromBlock: 0n,
+        toBlock: "latest",
+      });
+
+      setGrantedEvents(logs.map((log) => log.args.from));
+      toast.success("Admin Created");
+    } catch (err) {
+      toast.error(err.message);
+    }
+  };
+
+  const createSuperAdmin = async () => {
+    try {
+      const { request } = await prepareWriteContract({
+        address: contractAddressAdmins,
+        abi: AdminsABI,
+        functionName: "addSuperAdmin",
+        args: [_addr],
+      });
+
+      const { hash } = await writeContract(request);
+      const data = await waitForTransaction({
+        hash: hash,
+      });
+
+      getGrantedEvents();
+    } catch (err) {
+      toast.error(err.message);
+    }
+  };
+
   return (
-    <main className="flex justify-center items-center flex-col mb-80">
-      <form id="artistForm" className="flex justify-center items-center gap-4">
-        <label for="artistName" className="text-2xl">
-          Artist:
+    <main className="flex justify-center items-center flex-col mb-60 gap-8">
+      <form
+        id="adminForm"
+        className="flex justify-center items-center flex-col gap-4"
+      >
+        <label for="admin" className="text-2xl">
+          Admin
         </label>
-        <input
-          type="text"
-          id="artistName"
-          name="artistName"
-          className="p-1"
-          required
-        />
-        <button
-          type="submit"
-          className="bg-[#cef54b] text-[#15141a] rounded p-1"
-        >
-          Ajouter
-        </button>
+        <div className="flex gap-4">
+          <input
+            type="text"
+            id="artistName"
+            name="artistName"
+            className="text-[#15141a] p-1 rounded"
+            required
+          />
+          <button
+            type="submit"
+            className="bg-[#cef54b] text-[#15141a] rounded p-1"
+            onClick={createSuperAdmin}
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+
+      <form
+        id="artistForm"
+        className="flex justify-center items-center flex-col gap-4"
+      >
+        <label for="artist" className="text-2xl">
+          Artist
+        </label>
+        <div className="flex gap-4">
+          <input
+            type="text"
+            id="artistName"
+            name="artistName"
+            className="text-[#15141a] p-1 rounded"
+            required
+          />
+          <button
+            type="submit"
+            className="bg-[#cef54b] text-[#15141a] rounded p-1"
+          >
+            Submit
+          </button>
+        </div>
       </form>
     </main>
   );
---
+++ frontend/constants/index.js
@@ -0,0 +1,235 @@
+export const contractAddressAdmins =
+  "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+
+export const AdminsABI = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "initialOwner",
+        type: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "ERC1167FailedCreateClone",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+    ],
+    name: "OwnableInvalidOwner",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+    ],
+    name: "OwnableUnauthorizedAccount",
+    type: "error",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "enum Admins.Role",
+        name: "role",
+        type: "uint8",
+      },
+      {
+        indexed: false,
+        internalType: "address",
+        name: "contractt",
+        type: "address",
+      },
+    ],
+    name: "Granted",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "previousOwner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "OwnershipTransferred",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "newAdmin",
+        type: "address",
+      },
+    ],
+    name: "addAdmin",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_addr",
+        type: "address",
+      },
+    ],
+    name: "addSuperAdmin",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "adminRoles",
+    outputs: [
+      {
+        internalType: "enum Admins.Role",
+        name: "",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "adminsAccounts",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "adminsContracts",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "oldAdmin",
+        type: "address",
+      },
+    ],
+    name: "removeAdmin",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_addr",
+        type: "address",
+      },
+    ],
+    name: "removeSuperAdmin",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "renounceOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "transferOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
